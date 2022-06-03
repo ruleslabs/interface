5441f58862aba0ca44ae1f9a9ba930a049924d21
xshitaka <clanier.dev@gmail.com>
2022-06-03 20:22:42 +0200

    homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,19 @@
+import styled from 'styled-components'
+
+const Video = styled.video`
+  position: absolute;
+  top: 57px;
+  height: calc(100% - 57px + 128px); // 100% - header + footer
+  width: 100%;
+  object-fit: cover;
+
+  ${({ theme }) => theme.media.medium`
+    top: 62px;
+    height: calc(100% - 62px + 128px);
+    object-position: left;
+  `}
+`
+
 export default function Home() {
-  return null
+  return <Video src="https://videos.rules.art/mp4/homepage.mp4" playsInline loop autoPlay muted />
 }
