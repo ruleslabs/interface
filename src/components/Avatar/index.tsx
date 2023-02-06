3074f8104c5825ce95bff154894a74758e40bd2d
xshitaka <clanier.dev@gmail.com>
2023-02-06 17:36:35 +0100

    Fixed frozen pp on profile layout

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
 import styled from 'styled-components'
 
 const Image = styled.img`
@@ -13,6 +13,10 @@ interface AvatarProps extends React.HTMLAttributes<HTMLImageElement> {
 export default function Avatar({ fallbackSrc, src, ...props }: AvatarProps) {
   const [defaultSrc, setDefaultSrc] = useState(src)
   const onFallback = useCallback(() => setDefaultSrc(fallbackSrc), [fallbackSrc])
+
+  useEffect(() => {
+    setDefaultSrc(src)
+  }, [src])
 
   return <Image src={defaultSrc} {...props} onError={onFallback} />
 }
